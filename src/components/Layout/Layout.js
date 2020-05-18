761867602991c470060b602c73641af30ac87270
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-18 16:49:22 -0300

    creado un css para Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from "react";
+import "./Layout.css";
 
 const Layout = (props) => {
     return (
         <Fragment>
             <div>Toolbar, Sidedrawer, Backdrop</div>
-            <main>
+            <main className="Content">
                 {props.children}
             </main>
         </Fragment>
