9239e410aa052a37db14085ac7c59e4e35528b9a
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-18 16:48:55 -0300

    creado el componente BurguerBuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from "react";
+
+class BurguerBuilder extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>Hamburguesa</div>
+                <div>Controles de construcción</div>
+            </Fragment>
+        );
+    }
+}
+
+export default BurguerBuilder;
