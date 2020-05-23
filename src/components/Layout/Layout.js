6af1b67c2905c0519395bdf917dfbcd0794ed6a1
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-23 17:11:49 -0300

    ahora se puede cerrar el SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,28 @@
-import React, {Fragment} from "react";
+import React, {Fragment, Component} from "react";
 import estilos from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => {
-    return (
-        <Fragment>
-            <Toolbar />
-            <SideDrawer />
-            <main className={estilos.Content}>
-                {props.children}
-            </main>
-        </Fragment>
-    );
+class Layout extends Component {
+    state = {
+        mostrarSideDrawer: true,
+    }
+
+    sideDrawerCerradoHandler = () => {
+        this.setState({mostrarSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar />
+                <SideDrawer mostrar={this.state.mostrarSideDrawer} cerrado={this.sideDrawerCerradoHandler} />
+                <main className={estilos.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    }
 }
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import estilos from "./SideDrawer.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const SideDrawer = (props) => {
+    let clasesAgregadas = [estilos.SideDrawer, estilos.Close];
+    if (props.mostrar) {
+        clasesAgregadas[1] = estilos.Open;
+    }
     return (
-        <div className={estilos.SideDrawer}>
-            <div className={estilos.Logo}>
-                <Logo />
+        <Fragment>
+            <Backdrop mostrar={props.mostrar} clicked={props.cerrado} />
+            <div className={clasesAgregadas.join(" ")}>
+                <div className={estilos.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 }
 
