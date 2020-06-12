ed9fcb40e15efe734e4b7b4f845497b27c2a21d2
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-12 16:45:07 -0300

    ahora Input puede devolver un select tambien

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,15 +3,27 @@ import estilos from "./Input.module.css";
 
 const Input = (props) => {
     let inputElement = null;
-    switch (props.type) {
+    switch (props.elementType) {
         case "text":
-            inputElement = <input className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement =
+                <input className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
             break;
         case "textarea":
-            inputElement = <textarea className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement =
+                <textarea className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
+            break;
+        case "select":
+            inputElement = (
+                <select className={estilos.InputElement} {...props.elementConfig} value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>{option.displayValue}</option>
+                    ))}
+                </select> 
+            );
             break;
         default:
-            inputElement = <input className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement =
+                <input className={estilos.InputElement} {...props.elementConfig} value={props.value} />;
             break;
     }
     return (
