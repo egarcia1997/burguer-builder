803821529ddbfc35f459cfb944b7fe8e5f8c4bf5
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-25 16:25:16 -0300

    instalando redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from "./store/reducer";
 
+const store = createStore(reducer);
 const app = (
     <React.StrictMode>
         <BrowserRouter>
