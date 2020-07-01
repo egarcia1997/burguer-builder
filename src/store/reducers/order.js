7ee7313766382cb385007ade92487c8dc6064541
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-01 15:07:18 -0300

    creando el reducer order

---
+++ src/store/reducers/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+    orders: [],
+    cargando: false,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            }
+            return {
+                ...state,
+                cargando: false,
+                orders: state.orders.concat(action.newOrder),
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                cargando: false,
+            };
+        default:
+            return state;
+    }
+}
+
+export default reducer;
