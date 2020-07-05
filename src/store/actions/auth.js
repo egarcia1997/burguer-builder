2ab434806c6bbfb9995fbd63682560bb47a69159
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-05 17:05:55 -0300

    trayendo un token del backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "./actionTypes";
-import axios from "../../axios-orders";
+import Axios from "axios";
 
 const authSuccess = (authData) => {
     return {
@@ -24,10 +24,19 @@ const authStart = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
-        axios.get()
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true,
+        }
+        // esto es para REGISTRAR UN USUARIO
+        // NO PARA INICIAR SESIÓN
+        Axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBoQ1WAnxZ65FOWjAomu45_8wGXsIx6rT8", authData)
             .then(response => {
+                console.log(response);
                 dispatch(authSuccess());
             }).catch(error => {
+                console.log(error);
                 dispatch(authFail(error));
             });
     }
