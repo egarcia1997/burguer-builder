e82d5a70ac4fdf64b6d73724a96be9a14df5fe07
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-10 16:49:34 -0300

    protegiendo rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from "./containers/Layout/Layout";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
@@ -15,20 +15,37 @@ class App extends Component {
     }
 
     render () {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" component={BurgerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/" component={BurgerBuilder} />
+                </Switch>
+            );
+        }
         return (
             <div>
                 <Layout>
                     {/* no es necesario usar exact y Switch a la vez */}
-                    <Switch>
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/" component={BurgerBuilder} />
-                    </Switch>
+                    {routes}
                 </Layout>
             </div>
         );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null,
     }
 }
 
@@ -38,4 +55,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
