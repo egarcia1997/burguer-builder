313ffa28b85ea93b375019094218f67f09b793ce
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-12 16:18:31 -0300

    arreglando la redireccion a la pagina principal

--- src/App.js
+++ src/App.js
@@ -27,8 +27,10 @@ class App extends Component {
                 <Switch>
                     <Route path="/orders" component={Orders} />
                     <Route path="/checkout" component={Checkout} />
+                    <Route path="/auth" component={Auth} />
                     <Route path="/logout" component={Logout} />
                     <Route path="/" component={BurgerBuilder} />
+                    <Redirect to="/" />
                 </Switch>
             );
         }
