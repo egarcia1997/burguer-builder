a9ad8e3dd16e1204897e232e57f8e66d0dcf043a
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-24 21:59:08 -0300

    conectando el saga a redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import {createStore, applyMiddleware, compose, combineReducers} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+import {logout} from "./store/sagas/auth";
 
 const composeEnhancers = (process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
@@ -19,7 +21,11 @@ const rootReducer = combineReducers({
     auth: authReducer,
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+
+// sagaMiddleware.run(logout);
 
 const app = (
     <React.StrictMode>
