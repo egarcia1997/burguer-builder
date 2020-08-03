41c421c46c1f50a138e13aa3af36e47c50581479
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-03 15:51:51 -0300

    pasando los props en los routes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = props => {
 
     let routes = (
         <Switch>
-            <Route path="/auth" render={() => <Auth />} />
+            <Route path="/auth" render={props => <Auth {...props} />} />
             <Route path="/" component={BurgerBuilder} />
             <Redirect to="/" />
         </Switch>
@@ -31,9 +31,9 @@ const App = props => {
     if (props.isAuthenticated) {
         routes = (
             <Switch>
-                <Route path="/orders" render={() => <Orders />} />
-                <Route path="/checkout" render={() => <Checkout />} />
-                <Route path="/auth" render={() => <Auth />} />
+                <Route path="/orders" render={props => <Orders {...props} />} />
+                <Route path="/checkout" render={props => <Checkout {...props} />} />
+                <Route path="/auth" render={props => <Auth {...props} />} />
                 <Route path="/logout" component={Logout} />
                 <Route path="/" component={BurgerBuilder} />
                 <Redirect to="/" />
