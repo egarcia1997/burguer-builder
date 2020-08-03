635e1cb9201723d960eb675cec32474794229b73
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-03 15:31:02 -0300

    convirtiendo Layout a componente funcional

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,44 +1,36 @@
-import React, {Fragment, Component} from "react";
+import React, {Fragment, useState} from "react";
 import { connect } from "react-redux";
 import estilos from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-class Layout extends Component {
-    state = {
-        mostrarSideDrawer: false,
+const Layout = props => {
+    const [mostrarSideDrawer, setMostrarSideDrawer] = useState(false);
+
+    const sideDrawerCerradoHandler = () => {
+        setMostrarSideDrawer(false);
     }
 
-    sideDrawerCerradoHandler = () => {
-        this.setState({mostrarSideDrawer: false});
+    const sideDrawerToggleHandler = () => {
+        setMostrarSideDrawer(!mostrarSideDrawer);
     }
 
-    sideDrawerToggleHandler = () => {
-        // esta forma de usar setState es para evitar que pasen cosas raras
-        // porque es asincrono
-        this.setState((prevState) => {
-            return {mostrarSideDrawer : !prevState.mostrarSideDrawer}
-        });
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <Toolbar
-                    drawerToggleClicked={this.sideDrawerToggleHandler}
-                    isAuth={this.props.isAuthenticated}
-                />
-                <SideDrawer
-                    mostrar={this.state.mostrarSideDrawer}
-                    cerrado={this.sideDrawerCerradoHandler}
-                    isAuth={this.props.isAuthenticated}
-                />
-                <main className={estilos.Content}>
-                    {this.props.children}
-                </main>
-            </Fragment>
-        );
-    }
+    return (
+        <Fragment>
+            <Toolbar
+                drawerToggleClicked={sideDrawerToggleHandler}
+                isAuth={props.isAuthenticated}
+            />
+            <SideDrawer
+                mostrar={mostrarSideDrawer}
+                cerrado={sideDrawerCerradoHandler}
+                isAuth={props.isAuthenticated}
+            />
+            <main className={estilos.Content}>
+                {props.children}
+            </main>
+        </Fragment>
+    );
 }
 
 const mapStateToProps = state => {
