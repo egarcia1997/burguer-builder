90526fe4514e92345cd0a639c3635dc05ba25df7
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-03 16:54:40 -0300

    convirtiendo Modal a funcional

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,27 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment } from "react";
 import estilos from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
+const Modal = props => {
     // esto esta para evitar que se vuelva a renderizar el componente OrderSummary
     // cada vez que agrego ingredientes a la hamburguesa
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.mostrar !== this.props.mostrar || nextProps.children !== this.props.children;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps.mostrar !== this.props.mostrar || nextProps.children !== this.props.children;
+    // }
 
-    render() {
-        return (
-            <Fragment>
-                <Backdrop mostrar={this.props.mostrar} clicked={this.props.modalClosed} />
-                <div className={estilos.Modal} style={{
-                    transform: this.props.mostrar ? "translateY(0)" : "translateY(-100vh)",
-                    opacity: this.props.mostrar ? "1" : "0"
-                }}>
-                    {this.props.children}
-                </div>
-            </Fragment>
-        );
-    }
+    return (
+        <Fragment>
+            <Backdrop mostrar={props.mostrar} clicked={props.modalClosed} />
+            <div className={estilos.Modal} style={{
+                transform: props.mostrar ? "translateY(0)" : "translateY(-100vh)",
+                opacity: props.mostrar ? "1" : "0"
+            }}>
+                {props.children}
+            </div>
+        </Fragment>
+    );
 }
 
-export default Modal;
+export default React.memo(Modal, (prevProps, nextProps) =>
+    nextProps.mostrar === prevProps.mostrar && nextProps.children === prevProps.children
+);
